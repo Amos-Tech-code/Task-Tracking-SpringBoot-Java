bc0ca60faebf25ad1e17644f8efaa9e392b6ccb6
Amos-Tech-code <amosk51332@gmail.com>
2025-08-29 16:27:13 +0300

    first commit

---
+++ src/test/java/com/amos_tech_code/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amos_tech_code.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
